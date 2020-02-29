a3bce671798ab6599765ff6b4d0f9f7ffd82d3fb
electricerger <electricerger@gmail.com>
2020-02-29 10:09:07 -0500

    Initial commit

---
+++ Entrepreneur/Class1.cs
@@ -0,0 +1,33 @@
+﻿using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewValley;
+using StardewValley.Menus;
+using StardewValley.Events;
+using StardewValley.Quests;
+
+namespace Entrepreneur
+{
+    public class ModEntry : Mod
+    {
+        /*********
+        ** Public methods
+        *********/
+        /// <summary>The mod entry point, called after the mod is first loaded.</summary>
+        /// <param name="helper">Provides simplified APIs for writing mods.</param>
+        public override void Entry(IModHelper helper)
+        {
+            foreach (IContentPack contentPack in this.Helper.ContentPacks.GetOwned())
+            {
+                this.Monitor.Log($"Reading content pack: {contentPack.Manifest.Name} {contentPack.Manifest.Version} from {contentPack.DirectoryPath}");
+                helper.Events.GameLoop.SaveLoaded += GameLoop_SaveLoaded;
+            }
+        }
+
+        private void GameLoop_SaveLoaded(object sender, SaveLoadedEventArgs e)
+        {
+            Quest q = GoSomewhereQuest.getQuestFromId(1);
+            Game1.player.addQuest(1);
+            Game1.player.questLog.Add(q);
+        }
+    }
+}
---
+++ Timeturner/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Timeturner
+{
+    public class Class1
+    {
+    }
+}
